d66b001da67c85dec6fa40483c6dd0d9440427a3
Andrew Claudy <andrew.claudy@live.com>
2013-11-09 22:54:21 -0500

    Sets up empty C++ Project and Repo

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//Andrew Claudy & JT Broad
+//Intro to Algorithms w/ Dr. Yeager
+//Due 2355 on 09 Dec 2013
+//Description:
+//<insert description here>
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+#include <fstream>
+using std::ifstream;
+
+void main()
+{
+
+}
